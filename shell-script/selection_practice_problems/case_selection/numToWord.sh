dfd07550a14b271ac5a341329d087b92b804fe52
Saksham27 <geekysaksham27@gmail.com>
2020-03-13 14:18:08 +0530

    Added: take number and prints that in words

---
+++ shell-script/selection_practice_problems/case_selection/numToWord.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+read -p "Enter single digit number : " number
+if [ $number -ge 0 ] && [ $number -le 9 ]
+then
+	case $number in
+		1)
+			echo One;;
+		2)
+			echo Two;;
+		3)
+			echo Three;;
+		4)
+			echo Four;;
+		5)
+			echo Five;;
+		6)
+			echo Six;;
+		7)
+			echo Seven;;
+		8)
+			echo Eight;;
+		9)
+			echo Nine;;
+		*)
+			echo Zero;;
+	esac
+else
+	echo Enter valid single digit number
+fi
