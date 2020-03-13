4f834ed790329fa6d73a33e38297d82c3e170b6e
Saksham27 <geekysaksham27@gmail.com>
2020-03-13 09:12:55 +0530

    Refactor: Fixed indentation and spaces

--- shell-script/sequence_practice_problems/printDay.sh
+++ shell-script/sequence_practice_problems/printDay.sh
@@ -5,7 +5,7 @@ year=`echo $FULL_DATE | awk -F/ '{print $3}'`
 month=`echo $FULL_DATE | awk -F/ '{print $2}'`
 date=`echo $FULL_DATE | awk -F/ '{print $1}'`
 
-tempYear=$(( $year-(14-$month)/12))
+tempYear=$(( $year-(14-$month)/12 ))
 x=$(( $tempYear+$tempYear/4-$tempYear/100+$tempYear/400 ))
 tempMonth=$(( $month+12*((14-$month)/12)-2 ))
 tempDate=$(( ($date+$x+31*$tempMonth/12)%7 ))
