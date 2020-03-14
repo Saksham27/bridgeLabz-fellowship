7d0cb08879b9b223f23f7f4381584af5267f50e0
Saksham27 <geekysaksham27@gmail.com>
2020-03-14 15:50:24 +0530

    Added : function to check if 2 numbers are palindrome of each other

--- shell-script/function_practice_problems/checkPalindrome.sh
+++ shell-script/function_practice_problems/checkPalindrome.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+# function to get palindrome of a number
 function getPalindrome() {
 	local temp=$1
 	local sum=0
@@ -11,7 +12,18 @@ function getPalindrome() {
 	echo $sum
 }
 
+# function to check IF 2 numbers are palindorme of each other
+function checkPalindrome() {
+	if [ $1 -eq $2 ]
+	then
+		echo "Both the numbers are palindrome of each other"
+	else
+		echo "Numbers are not palindrome"
+	fi
+}
+
 read -p "Enter first number : " num1
 read -p "Enter second number : " num2
 
 num1Palindrome=$( getPalindrome $num1 )
+checkPalindrome $num1Palindrome $num2
