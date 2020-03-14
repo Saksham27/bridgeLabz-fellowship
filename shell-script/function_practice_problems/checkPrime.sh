1d99de503bf9341b0cff850bf5d26f47cdab6155
Saksham27 <geekysaksham27@gmail.com>
2020-03-14 16:08:04 +0530

    Added: script to check if number and it's palindrome are prime or not

--- shell-script/function_practice_problems/checkPrime.sh
+++ shell-script/function_practice_problems/checkPrime.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+# function to check if a number is prime
 function checkPrime() {
 	local count=0
 	for (( i=2; i<$1; i++ ))
@@ -17,5 +18,32 @@ function checkPrime() {
 	fi
 }
 
+# function to get palindrome of a number
+function getPalindrome() {
+	local temp=$1
+	local sum=0
+	while [ $temp -ne 0 ]
+	do
+		sum=$(( $(($sum*10+$temp%10)) ))
+		temp=$(( $temp/10 ))
+	done
+	echo $sum
+}
+
 read -p "Enter a number : " num
 isNumPrime=$( checkPrime $num )
+numPalindrome=$( getPalindrome $num )
+isNumPalindromePrime=$( checkPrime $numPalindrome )
+if [ $isNumPrime = true ]
+then
+	echo "Number is Prime"
+else
+	echo "Number is not prime"
+fi
+
+if [ $isNumPalindromePrime = true ]
+then
+	echo "Palindrome of number is also Prime"
+else
+	echo "Palindrome of number is not prime"
+fi
